89ac281b5475e9aa7b46af69dfd85a9dda614962
Ubuntu <ubuntu@ip-172-31-85-30.ec2.internal>
2019-09-10 18:54:12 +0000

    add files

--- quiz/.~c9_invoke_HJ549.py
+++ quiz/.~c9_invoke_HJ549.py
@@ -12,28 +12,11 @@ def ask_questions():
     
     with open("question.txt", "r") as file:
         lines = file.read().splitlines()
-    
+        
     for i, text in enumerate(lines):
         if i%2 == 0:
-            questions.append(text)
-        else:
-            answers.append(text)
-    
-    number_of_questions = len(questions)
-    questions_and_answers = zip(questions, answers)
-    
-    score = 0
-            
-    for question, answer in questions_and_answers:
-        guess = input(question + "> ")
-        if guess == answer:
-            score += 1
-            print("right!")
-            print(score)
-        else:
-             print("wrong!")
-    
-    print("You got {0} correct out of {1}".format(score, number_of_questions))
+            question
+        
     
 def add_question():
     print("")
@@ -54,7 +37,7 @@ def game_loop():
     while True:
         option = show_menu()
         if option == "1":
-            ask_questions()
+            print("You selected 'Ask questions'")
         elif option == "2":
             add_question()
         elif option == "3":
